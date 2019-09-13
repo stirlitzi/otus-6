887480fabea46328e3d1347c0f0b95fec4f8a76f
stirlitzi <33487154+stirlitzi@users.noreply.github.com>
2019-09-13 13:49:27 +0300

    Update test_matrix.cpp

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -5,16 +5,12 @@ TEST(TestMatrix, TestSize) {
 Matrix<int, -1> matrix; // бесконечная матрица int заполнена значениями -1
 ASSERT_EQ(matrix.size(), 0); // все ячейки свободны
 auto a = matrix[0][0];
-ASSERT_EQ(a, -1);
+ASSERT_EQ(static_cast<int>(a), -1);
 EXPECT_EQ(matrix.size(), 0);
-auto b = matrix[0][0];
-b = 256;
-ASSERT_EQ(matrix[0][0], 256);
-b = -1;
-ASSERT_EQ(matrix[0][0], -1);
+ASSERT_EQ(static_cast<int>(matrix[0][0]), -1);
 ASSERT_EQ(matrix.size(), 0);
 matrix[100][100] = 314;
-ASSERT_EQ(matrix[100][100], 314);
+ASSERT_EQ(static_cast<int>(matrix[100][100]), 314);
 EXPECT_EQ(matrix.size(), 1);
 }
 
