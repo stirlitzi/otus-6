2203a50b03cb24d63513484df437f0ef49575248
stirlitzi <33487154+stirlitzi@users.noreply.github.com>
2019-09-13 11:49:34 +0300

    Update test_matrix.cpp

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -7,7 +7,7 @@ ASSERT_EQ(matrix.size(), 0); // все ячейки свободны
 auto a = matrix[0][0];
 ASSERT_EQ(a, -1);
 EXPECT_EQ(matrix.size(), 0);
-auto& b = matrix[0][0];
+auto b = matrix[0][0];
 b = 256;
 ASSERT_EQ(matrix[0][0], 256);
 b = -1;
